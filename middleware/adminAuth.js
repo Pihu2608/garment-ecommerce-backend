ab9f3fc4f36b721aca874fdac2745cfdfd9e6a2b
Pihu2608 <nitinmalviya2206@gmail.com>
2026-01-04 18:18:16 +0530

    Update adminAuth.js

--- middleware/adminAuth.js
+++ middleware/adminAuth.js
@@ -1,10 +1,10 @@
 const jwt = require("jsonwebtoken");
 
-module.exports = function (req, res, next) {
+module.exports = function adminAuth(req, res, next) {
   const authHeader = req.headers.authorization;
 
-  if (!authHeader) {
-    return res.status(401).json({ message: "No token provided" });
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized" });
   }
 
   const token = authHeader.split(" ")[1];
@@ -14,6 +14,6 @@ module.exports = function (req, res, next) {
     req.admin = decoded;
     next();
   } catch (err) {
-    return res.status(401).json({ message: "Invalid or expired token" });
+    return res.status(401).json({ message: "Invalid / Expired token" });
   }
 };

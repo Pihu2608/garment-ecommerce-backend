7a2fcac318ec70f3d912729562068f6e8f96ff36
Pihu2608 <nitinmalviya2206@gmail.com>
2026-01-04 12:38:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(express.json());
 
 // ROUTES
 const orderRoutes = require("./routes/orderRoutes");
-
 app.use("/api/orders", orderRoutes);
 
 // DB
@@ -18,9 +17,8 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log("✅ MongoDB Connected");
 });
 
-
 // SERVER
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });

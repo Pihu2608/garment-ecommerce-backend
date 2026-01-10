4adec1eb7718124e15aba1371750d9a513bb9860
Pihu2608 <nitinmalviya2206@gmail.com>
2026-01-10 21:15:51 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  category: String,
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
   image: String,
+  category: String,
   description: String,
-  stock: Number
-});
+  stock: { type: Number, default: 1 }
+}, { timestamps: true });
 
 module.exports = mongoose.model("Product", productSchema);

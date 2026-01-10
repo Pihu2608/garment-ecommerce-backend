5af7640ad1f9d7134b27dd99eba199545745a83b
Pihu2608 <nitinmalviya2206@gmail.com>
2026-01-10 20:54:33 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,23 +2,21 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../models/Product");
 
-// ADD PRODUCT
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 router.post("/", async (req, res) => {
   try {
     const product = await Product.create(req.body);
     res.json({ success: true, product });
   } catch (err) {
     res.status(500).json({ success: false, message: err.message });
-  }
-});
-
-// GET ALL PRODUCTS
-router.get("/", async (req, res) => {
-  try {
-    const products = await Product.find().sort({ createdAt: -1 });
-    res.json(products);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
   }
 });
 

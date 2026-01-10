011780a0ab7ada5619c45d21a30059586d44d611
Pihu2608 <nitinmalviya2206@gmail.com>
2026-01-10 20:40:59 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,1 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const productCtrl = require("../controllers/productController");
+const verifyAdmin = require("../middleware/verifyAdmin");
+
+// PUBLIC
+router.get("/", productCtrl.getProducts);
+router.get("/:id", productCtrl.getSingleProduct);
+
+// ADMIN
+router.post("/", verifyAdmin, productCtrl.addProduct);
+router.put("/:id", verifyAdmin, productCtrl.updateProduct);
+router.delete("/:id", verifyAdmin, productCtrl.deleteProduct);
+
+module.exports = router;
 
